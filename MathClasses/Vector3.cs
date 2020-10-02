64ec024c9ebd4d59f95d77e2f64613631fc41e7a
Ryan Millen <ryantheinventor@gmail.com>
2020-10-02 11:23:10 -0700

    Init commit and Vector3

---
+++ MathClasses/Vector3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public class Vector3
+    {
+        public float x, y, z;
+
+
+        public Vector3()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector3 operator +(Vector3 left, Vector3 right)
+            => new Vector3(left.x + right.x, left.y + right.y, left.z + right.z);
+
+        public static Vector3 operator -(Vector3 left, Vector3 right)
+            => new Vector3(left.x - right.x, left.y - right.y, left.z - right.z);
+        public static Vector3 operator *(float left, Vector3 right)
+            => new Vector3(left * right.x, left * right.y, left * right.z);
+        public static Vector3 operator *(Vector3 left, float right)
+            => new Vector3(right * left.x, right * left.y, right * left.z);
+
+        public float Dot(Vector3 other)
+            => x * other.x + y * other.y + z * other.z;
+
+        public Vector3 Cross(Vector3 other)
+            => new Vector3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
+
+        public float Magnitude()
+            => (float)Math.Sqrt(x * x + y * y + z * z);
+        
+
+        public void Normalize()
+        {
+            float mag = Magnitude();
+            x /= mag;
+            y /= mag;
+            z /= mag;
+        }
+
+    }
+}
---
+++ MathClasses/Vector4.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public class Vector4
+    {
+        public float x, y, z, w;
+
+        public Vector4()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+            w = 0;
+        }
+
+        public Vector4(float x, float y, float z, float w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public static Vector4 operator +(Vector4 left, Vector4 right)
+            => new Vector4(left.x + right.x, left.y + right.y, left.z + right.z, left.w+right.w);
+
+        public static Vector4 operator -(Vector4 left, Vector4 right)
+            => new Vector4();
+        public static Vector4 operator *(Vector4 left, Vector4 right)
+            => new Vector4();
+        public static Vector4 operator /(Vector4 left, Vector4 right)
+            => new Vector4();
+        public static Vector4 operator *(float left, Vector4 right)
+            => new Vector4();
+        public static Vector4 operator *(Vector4 left, float right)
+            => new Vector4();
+
+        public float Dot(Vector4 other)
+        {
+            return 0f;
+        }
+
+        public Vector4 Cross(Vector4 other)
+        {
+            return new Vector4();
+        }
+
+        public float Magnitude()
+        {
+            return 0f;
+        }
+
+        public void Normalize()
+        {
+
+        }
+
+
+    }
+}
